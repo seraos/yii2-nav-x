eddcf926d7bbd878a14e92b4712853c6042588b5
rogerio <Rogrio Soares>
2025-04-23 10:03:22 -0300

    bugs fix Declaration of kartik\nav\NavXTrait::init() must be compatible with yii\bootstrap5\Nav::init(): void

--- src/NavXTrait.php
+++ src/NavXTrait.php
@@ -27,7 +27,7 @@ trait NavXTrait
      * @inheritdoc
      * @throws InvalidConfigException
      */
-    public function init()
+    public function init() :void
     {
         $this->checkDependency();
         parent::init();
